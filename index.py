698d23edf78f75e829e7b8f0ac253795c0149bf0
zvdm <zvdm@protonmail.com>
2019-08-30 14:00:55 +0300

    First steps, added basic classes, functions, templates

--- index.py
+++ index.py
@@ -1,36 +1,99 @@
-import aiohttp
+import asyncio
+import asyncpg
+import datetime
+import jinja2
+import aiohttp_jinja2
+import uuid
+from cryptography import fernet
 from aiohttp import web
-
-# async def hello(request):
-#     return web.Response(text="Hello, world")
-#
-# app = web.Application()
-# app.add_routes([web.get('/', hello)])
-#
-# web.run_app(app)
+from aiohttp_session import setup, get_session, session_middleware
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
+from aiohttp_security import authorized_userid
 
 routes = web.RouteTableDef()
 
 
-@routes.get('/')
-async def index(request):
-    return web.HTTPPermanentRedirect('/user')
+async def current_user_ctx_processor(request):
+    userid = await authorized_userid(request)
+    is_anonymous = not bool(userid)
+    return {'current_user': {'is_anonymous': is_anonymous}}
 
 
-@routes.get('/user')
-async def user(request):
-    return web.Response(text='Hello, Anonymous!\nPlease login or sign up using your special key!')
+@routes.view('/')
+class Index(web.View):
+    async def get(self):
+        return web.HTTPPermanentRedirect('/login')
 
 
-@routes.get('/album')
-async def album(request):
-    return web.Response(text='I\'m working on this page')
+@routes.view('/login')
+class Login(web.View):
+    @aiohttp_jinja2.template('login.html')
+    async def get(self):
+        print(self.request.cookies)
+        # session = await get_session(request)
+        # print(session)
+        return {'user': 'John', 'album': '/album'}
+
+    async def post(self):
+        data = await self.request.post()
+        login = data['login']
+        pwd = data['password']
+        if login == 'user' and pwd == '1234':
+            return web.Response(text="You have logged in")
+        raise web.HTTPFound('/login')
 
 
-@routes.get('/track')
-async def album(request):
-    return web.Response(text='I\'m working on this page')
+@routes.view('/album')
+class Album(web.View):
+    async def get(self):
+        return web.Response(text='I\'m working on this page')
+
+
+@routes.view('/track')
+class Track(web.View):
+    async def get(self):
+        return web.Response(text='I\'m working on this page "track"')
+
+
+# @routes.get(r'/{not_found:\d+}')
+# async def not_found(request):
+#     raise web.HTTPFound('/')
+
 
 app = web.Application()
+aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates'))
+app['static_root_url'] = 'static'
+
 app.add_routes(routes)
+
+
+async def main():
+    conn = await asyncpg.connect('postgresql://mmapp@localhost/mmappdb', password='mmapp1234')
+    await conn.execute('''
+        CREATE TABLE IF NOT EXISTS Users(
+            id SERIAL PRIMARY KEY, 
+            name TEXT UNIQUE, 
+            api_key UUID UNIQUE, 
+            create_date TIMESTAMP,
+            active_status BOOL
+        );
+        
+        CREATE TABLE IF NOT EXISTS Albums(
+            id SERIAL PRIMARY KEY,
+            name TEXT,
+            created_date TIMESTAMP,
+            user_id INTEGER
+        );
+        
+        CREATE TABLE IF NOT EXISTS Tracks(
+            id SERIAL PRIMARY KEY,
+            name TEXT,
+            created_date TIMESTAMP,
+            album_id INTEGER
+        );
+    ''')
+
+    await conn.close()
+
+asyncio.get_event_loop().run_until_complete(main())
 web.run_app(app)
